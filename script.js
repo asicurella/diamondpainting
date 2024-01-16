4818a841e5ac0f4cf7fc00b004901824a39461be
Aliya <aliyajsicurella@gmail.com>
2024-01-16 17:17:18 -0500

    Remove / from url for hero image

---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', function () {
+    // Hide the preloader container after the page has fully loaded
+ document.querySelector('.preloader-container').style.display = 'none';
+});
+ 
